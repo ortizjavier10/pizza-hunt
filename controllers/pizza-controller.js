1f575333a340c7547d224ccce2b037c25c5ad247
JAVI <ortiz.javier10@gmail.com>
2020-09-30 18:44:59 -0400

    CRUD pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -27,6 +27,39 @@ const pizzaController = {
                 res.status(400).json(err);
             });
     },
+
+    // creatPizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.sttaus(400).json(err));
+    },
+
+    // update Pizza by id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true})
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(400).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // delete pizza
+    deletePizza({ params }, res) {
+        Pizza.findByIdAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ mesage: 'No pizza found wit this id! '});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.sttaus(400).json(err));
+    }
 }
 
 module.exports = pizzaController;
